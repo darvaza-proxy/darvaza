f91b9697c1ebaa6dc0fe2fda1c444e21decbaa71
Nagy Károly Gábriel <k@jpi.io>
2021-09-08 14:30:41 +0300

    tls/sni: rework version 2

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/creasty/defaults v1.5.1
 	github.com/hashicorp/hcl/v2 v2.10.1
 	github.com/spf13/cobra v1.2.1
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- tls/sni/sni.go
+++ tls/sni/sni.go
@@ -1,201 +1,341 @@
 package sni
 
 import (
-	"encoding/binary"
-	"fmt"
-	"io"
+	"strings"
+
+	"golang.org/x/crypto/cryptobyte"
 )
+
+// GetInfo returns a pointer to a ClientHelloInfo:
+// type ClientHelloInfo struct {
+//	Vers                             uint16
+//	CipherSuites                     []uint16
+//	CompressionMethods               []uint8
+//	ServerName                       string
+//	SupportedSignatureAlgorithms     []SignatureScheme
+//	SupportedSignatureAlgorithmsCert []SignatureScheme
+//	SupportedVersions                []uint16
+//}
+func GetInfo(buf []byte) *ClientHelloInfo {
+	n := len(buf)
+	if n <= 5 {
+
+		return nil
+	}
+
+	// tls record type
+	if recordType(buf[0]) != recordTypeHandshake {
+		return nil
+	}
+
+	// tls major Version
+	if buf[1] != 3 {
+		return nil
+	}
+
+	// handshake message type
+	if uint8(buf[5]) != typeClientHello {
+		return nil
+	}
+
+	// parse client hello message
+	msg := &ClientHelloInfo{}
+
+	ret := msg.unmarshal(buf[5:n])
+	if !ret {
+		return nil
+	}
+	// Lower than TLS13,
+	if len(msg.SupportedVersions) == 0 {
+		msg.SupportedVersions = append(msg.SupportedVersions, msg.Vers)
+	}
+	return msg
+}
+
+// below this line is an edited portion of
+// $GOROOT/src/crypto/tls/handshake_messages.go version 1.17
+//
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 type ClientHelloInfo struct {
-	Version string
-	SNI     string
-	Valid   bool
+	raw                              []byte
+	Vers                             uint16
+	random                           []byte
+	sessionId                        []byte
+	CipherSuites                     []uint16
+	CompressionMethods               []uint8
+	ServerName                       string
+	ocspStapling                     bool
+	supportedCurves                  []CurveID
+	supportedPoints                  []uint8
+	ticketSupported                  bool
+	sessionTicket                    []uint8
+	SupportedSignatureAlgorithms     []SignatureScheme
+	SupportedSignatureAlgorithmsCert []SignatureScheme
+	secureRenegotiationSupported     bool
+	secureRenegotiation              []byte
+	alpnProtocols                    []string
+	scts                             bool
+	SupportedVersions                []uint16
+	cookie                           []byte
+	keyShares                        []keyShare
+	earlyData                        bool
+	pskModes                         []uint8
+	pskIdentities                    []pskIdentity
+	pskBinders                       [][]byte
 }
 
-const (
-	tlsHandshakeType   = 22
-	tlsSNIExtension    = 0
-	tlsClientHelloType = 1
-	tls10              = 769
-	tls11              = 770
-	tls12              = 771
-	tlsMaxLength       = 16384
-	tlsSessionIDLength = 32
-)
+func readUint8LengthPrefixed(s *cryptobyte.String, out *[]byte) bool {
+	return s.ReadUint8LengthPrefixed((*cryptobyte.String)(out))
+}
 
-var chi = new(ClientHelloInfo)
+func readUint16LengthPrefixed(s *cryptobyte.String, out *[]byte) bool {
+	return s.ReadUint16LengthPrefixed((*cryptobyte.String)(out))
+}
 
-// Extracts SNI and version information from a TLS handshake.
-func GetInfo(r io.Reader) (*ClientHelloInfo, error) {
-	if err := parseRecord(r); err != nil {
-		chi.reset()
-		return chi, err
+func (m *ClientHelloInfo) unmarshal(data []byte) bool {
+	*m = ClientHelloInfo{raw: data}
+	s := cryptobyte.String(data)
+
+	if !s.Skip(4) || // message type and uint24 length field
+		!s.ReadUint16(&m.Vers) || !s.ReadBytes(&m.random, 32) ||
+		!readUint8LengthPrefixed(&s, &m.sessionId) {
+		return false
 	}
 
-	b, err := decodeVector(r, 2)
-	if err != nil {
-		// No extension (not an error).
-		if err == io.EOF {
-			err = nil
+	var CipherSuites cryptobyte.String
+	if !s.ReadUint16LengthPrefixed(&CipherSuites) {
+		return false
+	}
+	m.CipherSuites = []uint16{}
+	m.secureRenegotiationSupported = false
+	for !CipherSuites.Empty() {
+		var suite uint16
+		if !CipherSuites.ReadUint16(&suite) {
+			return false
 		}
-		chi.reset()
-		return chi, err
+		if suite == scsvRenegotiation {
+			m.secureRenegotiationSupported = true
+		}
+		m.CipherSuites = append(m.CipherSuites, suite)
 	}
 
-	// Loop over the extensions.
-	for len(b) >= 4 {
-		extType := binary.BigEndian.Uint16(b[:2])
-		length := binary.BigEndian.Uint16(b[2:4])
-		b = b[4:]
+	if !readUint8LengthPrefixed(&s, &m.CompressionMethods) {
+		return false
+	}
 
-		if extType == tlsSNIExtension {
-			chi.SNI, err = parseSNI(b[:length])
-			if err != nil {
-				chi.reset()
-				break
+	if s.Empty() {
+		// ClientHello is optionally followed by extension data
+		return true
+	}
+
+	var extensions cryptobyte.String
+	if !s.ReadUint16LengthPrefixed(&extensions) || !s.Empty() {
+		return false
+	}
+
+	for !extensions.Empty() {
+		var extension uint16
+		var extData cryptobyte.String
+		if !extensions.ReadUint16(&extension) ||
+			!extensions.ReadUint16LengthPrefixed(&extData) {
+			return false
+		}
+
+		switch extension {
+		case extensionServerName:
+			// RFC 6066, Section 3
+			var nameList cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&nameList) || nameList.Empty() {
+				return false
 			}
-		}
-		b = b[length:]
-	}
-	chi.Valid = true
-	return chi, err
-}
-
-func parseRecord(r io.Reader) error {
-	var record struct {
-		Type          uint8
-		Major         uint8
-		Minor         uint8
-		Length        uint16
-		MessageType   uint8
-		MessageLength [3]byte
-		Version       uint16
-		Random        [32]byte
-	}
-	if err := binary.Read(r, binary.BigEndian, &record); err != nil {
-		chi.reset()
-		return err
-	}
-
-	if record.Type != tlsHandshakeType {
-		chi.reset()
-		return fmt.Errorf("not a TLS handshake")
-	}
-
-	if record.Length > tlsMaxLength {
-		chi.reset()
-		return fmt.Errorf("TLS record length exceed maximum (%d > 2^14)", record.Length)
-	}
-	if record.MessageType != tlsClientHelloType {
-		chi.reset()
-		return fmt.Errorf("not a ClientHello message (%d)", record.MessageType)
-	}
-
-	chi.Version = fmt.Sprintf("%d.%d", record.Major, record.Minor)
-
-	// one byte SessionID
-	b, err := decodeVector(r, 1)
-	if err != nil {
-		chi.reset()
-		return fmt.Errorf("could not read ClientHello session ID (%s)", err)
-	}
-
-	if len(b) > tlsSessionIDLength {
-		chi.reset()
-		return fmt.Errorf("SessionID is bigger than allowed")
-	}
-
-	// two bytes cipher suites.
-	b, err = decodeVector(r, 2)
-	if err != nil {
-		chi.reset()
-		return err
-	}
-	if len(b) < 2 || len(b)%2 != 0 {
-		chi.reset()
-		return fmt.Errorf("ClientHello cipher suites has an invalid length (%d)", len(b))
-	}
-
-	// one byte compression methods.
-	b, err = decodeVector(r, 1)
-	if err != nil {
-		chi.reset()
-		return err
-	}
-	if len(b) < 1 {
-		chi.reset()
-		return fmt.Errorf("invalid length %d compression methods", len(b))
-	}
-
-	return nil
-}
-
-// Parse the SNI from an SNI extension.
-func parseSNI(b []byte) (string, error) {
-	if len(b) < 2 {
-		return "", fmt.Errorf("SNI extension is empty")
-	}
-
-	length := binary.BigEndian.Uint16(b[:2])
-	if int(length) > len(b[2:]) {
-		chi.reset()
-		return "", fmt.Errorf("SNI extension is too short")
-	}
-
-	b = b[2 : 2+length]
-
-	for len(b) >= 3 {
-		nameType := b[0]
-		vectLength := binary.BigEndian.Uint16(b[1:3])
-		if int(vectLength) > len(b[3:]) {
-			chi.reset()
-			return "", fmt.Errorf("SNI vector is too short")
-		}
-
-		if nameType != 0 {
-			b = b[3+vectLength:]
+			for !nameList.Empty() {
+				var nameType uint8
+				var serverName cryptobyte.String
+				if !nameList.ReadUint8(&nameType) ||
+					!nameList.ReadUint16LengthPrefixed(&serverName) ||
+					serverName.Empty() {
+					return false
+				}
+				if nameType != 0 {
+					continue
+				}
+				if len(m.ServerName) != 0 {
+					// Multiple names of the same name_type are prohibited.
+					return false
+				}
+				m.ServerName = string(serverName)
+				// An SNI value may not include a trailing dot.
+				if strings.HasSuffix(m.ServerName, ".") {
+					return false
+				}
+			}
+		case extensionStatusRequest:
+			// RFC 4366, Section 3.6
+			var statusType uint8
+			var ignored cryptobyte.String
+			if !extData.ReadUint8(&statusType) ||
+				!extData.ReadUint16LengthPrefixed(&ignored) ||
+				!extData.ReadUint16LengthPrefixed(&ignored) {
+				return false
+			}
+			m.ocspStapling = statusType == statusTypeOCSP
+		case extensionSupportedCurves:
+			// RFC 4492, sections 5.1.1 and RFC 8446, Section 4.2.7
+			var curves cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&curves) || curves.Empty() {
+				return false
+			}
+			for !curves.Empty() {
+				var curve uint16
+				if !curves.ReadUint16(&curve) {
+					return false
+				}
+				m.supportedCurves = append(m.supportedCurves, CurveID(curve))
+			}
+		case extensionSupportedPoints:
+			// RFC 4492, Section 5.1.2
+			if !readUint8LengthPrefixed(&extData, &m.supportedPoints) ||
+				len(m.supportedPoints) == 0 {
+				return false
+			}
+		case extensionSessionTicket:
+			// RFC 5077, Section 3.2
+			m.ticketSupported = true
+			extData.ReadBytes(&m.sessionTicket, len(extData))
+		case extensionSignatureAlgorithms:
+			// RFC 5246, Section 7.4.1.4.1
+			var sigAndAlgs cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&sigAndAlgs) || sigAndAlgs.Empty() {
+				return false
+			}
+			for !sigAndAlgs.Empty() {
+				var sigAndAlg uint16
+				if !sigAndAlgs.ReadUint16(&sigAndAlg) {
+					return false
+				}
+				m.SupportedSignatureAlgorithms = append(
+					m.SupportedSignatureAlgorithms, SignatureScheme(sigAndAlg))
+			}
+		case extensionSignatureAlgorithmsCert:
+			// RFC 8446, Section 4.2.3
+			var sigAndAlgs cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&sigAndAlgs) || sigAndAlgs.Empty() {
+				return false
+			}
+			for !sigAndAlgs.Empty() {
+				var sigAndAlg uint16
+				if !sigAndAlgs.ReadUint16(&sigAndAlg) {
+					return false
+				}
+				m.SupportedSignatureAlgorithmsCert = append(
+					m.SupportedSignatureAlgorithmsCert, SignatureScheme(sigAndAlg))
+			}
+		case extensionRenegotiationInfo:
+			// RFC 5746, Section 3.2
+			if !readUint8LengthPrefixed(&extData, &m.secureRenegotiation) {
+				return false
+			}
+			m.secureRenegotiationSupported = true
+		case extensionALPN:
+			// RFC 7301, Section 3.1
+			var protoList cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&protoList) || protoList.Empty() {
+				return false
+			}
+			for !protoList.Empty() {
+				var proto cryptobyte.String
+				if !protoList.ReadUint8LengthPrefixed(&proto) || proto.Empty() {
+					return false
+				}
+				m.alpnProtocols = append(m.alpnProtocols, string(proto))
+			}
+		case extensionSCT:
+			// RFC 6962, Section 3.3.1
+			m.scts = true
+		case extensionSupportedVersions:
+			// RFC 8446, Section 4.2.1
+			var VersList cryptobyte.String
+			if !extData.ReadUint8LengthPrefixed(&VersList) || VersList.Empty() {
+				return false
+			}
+			for !VersList.Empty() {
+				var Vers uint16
+				if !VersList.ReadUint16(&Vers) {
+					return false
+				}
+				m.SupportedVersions = append(m.SupportedVersions, Vers)
+			}
+		case extensionCookie:
+			// RFC 8446, Section 4.2.2
+			if !readUint16LengthPrefixed(&extData, &m.cookie) ||
+				len(m.cookie) == 0 {
+				return false
+			}
+		case extensionKeyShare:
+			// RFC 8446, Section 4.2.8
+			var clientShares cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&clientShares) {
+				return false
+			}
+			for !clientShares.Empty() {
+				var ks keyShare
+				if !clientShares.ReadUint16((*uint16)(&ks.group)) ||
+					!readUint16LengthPrefixed(&clientShares, &ks.data) ||
+					len(ks.data) == 0 {
+					return false
+				}
+				m.keyShares = append(m.keyShares, ks)
+			}
+		case extensionEarlyData:
+			// RFC 8446, Section 4.2.10
+			m.earlyData = true
+		case extensionPSKModes:
+			// RFC 8446, Section 4.2.9
+			if !readUint8LengthPrefixed(&extData, &m.pskModes) {
+				return false
+			}
+		case extensionPreSharedKey:
+			// RFC 8446, Section 4.2.11
+			if !extensions.Empty() {
+				return false // pre_shared_key must be the last extension
+			}
+			var identities cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&identities) || identities.Empty() {
+				return false
+			}
+			for !identities.Empty() {
+				var psk pskIdentity
+				if !readUint16LengthPrefixed(&identities, &psk.label) ||
+					!identities.ReadUint32(&psk.obfuscatedTicketAge) ||
+					len(psk.label) == 0 {
+					return false
+				}
+				m.pskIdentities = append(m.pskIdentities, psk)
+			}
+			var binders cryptobyte.String
+			if !extData.ReadUint16LengthPrefixed(&binders) || binders.Empty() {
+				return false
+			}
+			for !binders.Empty() {
+				var binder []byte
+				if !readUint8LengthPrefixed(&binders, &binder) ||
+					len(binder) == 0 {
+					return false
+				}
+				m.pskBinders = append(m.pskBinders, binder)
+			}
+		default:
+			// Ignore unknown extensions.
 			continue
 		}
 
-		return string(b[3 : 3+vectLength]), nil
-	}
-
-	// No DNS-based SNI.
-	return "", nil
-}
-
-func decodeVector(r io.Reader, l uint) ([]byte, error) {
-	rawLen := make([]byte, l)
-	if err := binary.Read(r, binary.BigEndian, &rawLen); err != nil {
-		// No data to read. This can be valid.
-		if err == io.EOF {
-			chi.reset()
-			return nil, err
+		if !extData.Empty() {
+			return false
 		}
-		chi.reset()
-		return nil, fmt.Errorf("could not read the vector length (%s)", err)
 	}
 
-	var length uint = 0
-	for _, b := range rawLen {
-		length = (length << 8) + uint(b)
-	}
-
-	if length == 0 {
-		chi.reset()
-		return nil, nil
-	}
-
-	data := make([]byte, length)
-	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
-		chi.reset()
-		return nil, fmt.Errorf("could not read the vector data (%s)", err)
-	}
-
-	return data, nil
-}
-func (c *ClientHelloInfo) reset() {
-	c.SNI = ""
-	c.Valid = false
-	c.Version = ""
+	return true
 }
