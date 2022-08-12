7c234ba446494b08fc492558474326f386d927b1
Alejandro Mery <amery@jpi.io>
2022-08-12 22:11:25 +0000

    shared/net: introduce ListenConfig.WithUpgrader() helper
    this is intended to allow us to use the multi-address helpers
    in combination with tableflip
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ shared/net/upgrader.go
@@ -0,0 +1,78 @@
+package net
+
+import (
+	"net"
+)
+
+// ListenUpgrader represents a tool that keep account of listening ports
+// but allows us to provide our own helper to do the last step
+type ListenUpgrader interface {
+	ListenWithCallback(network, addr string,
+		callback func(network, addr string) (net.Listener, error)) (net.Listener, error)
+	ListenPacketWithCallback(network, addr string,
+		callback func(netowkr, addr string) (net.PacketConn, error)) (net.PacketConn, error)
+}
+
+// ListenUpgraderConfig represents an object equivalent to a ListenConfig but
+// using a ListenUpgrader in the middle
+type ListenUpgraderConfig struct {
+	conf ListenConfig
+	upg  ListenUpgrader
+}
+
+// WithUpgrader creates a new ListenConfig using the provided ListenUpgrader
+func (lc ListenConfig) WithUpgrader(upg ListenUpgrader) *ListenUpgraderConfig {
+	return &ListenUpgraderConfig{
+		conf: lc,
+		upg:  upg,
+	}
+}
+
+// Listen acts like the standard net.Listen but using our ListenConfig and via
+// an Upgrader tool
+func (lu ListenUpgraderConfig) Listen(network, addr string) (net.Listener, error) {
+	return lu.upg.ListenWithCallback(network, addr, lu.conf.Listen)
+}
+
+// ListenPacket acts like the standard net.ListenPacket but using our
+// ListenConfig and via an Upgrader tool
+func (lu ListenUpgraderConfig) ListenPacket(network, addr string) (net.PacketConn, error) {
+	return lu.upg.ListenPacketWithCallback(network, addr, lu.conf.ListenPacket)
+}
+
+// ListenAll acts like Listen but on a list of addresses
+func (lu ListenUpgraderConfig) ListenAll(network string, addrs []string) ([]net.Listener, error) {
+	out := make([]net.Listener, 0, len(addrs))
+
+	for _, addr := range addrs {
+		lsn, err := lu.Listen(network, addr)
+		if err != nil {
+			for _, lsn := range out {
+				_ = lsn.Close()
+			}
+			return nil, err
+		}
+		out = append(out, lsn)
+	}
+
+	return out, nil
+}
+
+// ListenAllPacket acts like ListenPacket but on a list of addresses
+func (lu ListenUpgraderConfig) ListenAllPacket(network string, addrs []string) (
+	[]net.PacketConn, error) {
+	out := make([]net.PacketConn, 0, len(addrs))
+
+	for _, addr := range addrs {
+		lsn, err := lu.ListenPacket(network, addr)
+		if err != nil {
+			for _, lsn := range out {
+				_ = lsn.Close()
+			}
+			return nil, err
+		}
+		out = append(out, lsn)
+	}
+
+	return out, nil
+}
