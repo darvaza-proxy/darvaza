4bee0acf70a1e85d23e52cd755f1d0472277dc2a
Alejandro Mery <amery@jpi.io>
2022-12-31 17:44:32 +0000

    shared/storage/certpool: introduce PoolBuffer
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ shared/storage/certpool/poolbuffer_cert.go
@@ -0,0 +1,86 @@
+package certpool
+
+import (
+	"crypto/x509"
+
+	"github.com/darvaza-proxy/darvaza/shared/x509utils"
+)
+
+type pbCerts struct {
+	certs map[Hash]*pbCertData
+	pool  *CertPool
+}
+
+type pbCertData struct {
+	Filename string
+	Cert     *x509.Certificate
+
+	Hash Hash
+	Pub  x509utils.PublicKey
+}
+
+func (m *pbCerts) Reset() {
+	m.certs = nil
+	m.pool = nil
+}
+
+func (m *pbCerts) AddCert(fn string, cert *x509.Certificate) bool {
+	if cert == nil {
+		// NOOP
+		return false
+	} else if m.certs == nil {
+		// first
+		m.certs = make(map[Hash]*pbCertData)
+	}
+
+	hash := HashCert(cert)
+	if _, ok := m.certs[hash]; ok {
+		// duplicate
+		return false
+	}
+
+	// new
+	m.certs[hash] = &pbCertData{
+		Filename: fn,
+		Cert:     cert,
+		Hash:     hash,
+		Pub:      cert.PublicKey.(x509utils.PublicKey),
+	}
+	m.pool = nil
+	return true
+}
+
+func (m *pbCerts) Pool() *CertPool {
+	pool := m.pool
+	if pool == nil {
+		pool = &CertPool{}
+		if m.certs != nil {
+			for _, cd := range m.certs {
+				pool.addCertUnsafe(cd.Hash, "", cd.Cert)
+			}
+		}
+		m.pool = pool
+	}
+	return pool
+}
+
+func (m *pbCerts) Count() int {
+	return len(m.pool.hashed)
+}
+
+func (m *pbCerts) Export() *x509.CertPool {
+	return m.Pool().Export()
+}
+
+func (pb *PoolBuffer) addCertUnlocked(fn string, cert *x509.Certificate) error {
+	var pool *pbCerts
+
+	if cert.IsCA {
+		pool = &pb.roots
+	} else {
+		pool = &pb.certs
+	}
+
+	pool.AddCert(fn, cert)
+	return nil
+}
---
+++ shared/storage/certpool/poolbuffer_keys.go
@@ -0,0 +1,61 @@
+package certpool
+
+import (
+	"container/list"
+
+	"github.com/darvaza-proxy/darvaza/shared/x509utils"
+)
+
+type pbKeys struct {
+	keys  *list.List
+	count int
+}
+
+func (p *pbKeys) Reset() {
+	p.keys = list.New()
+	p.count = 0
+}
+
+func (p *pbKeys) Count() int {
+	return p.count
+}
+
+type pbKeyData struct {
+	filename string
+	pk       x509utils.PrivateKey
+}
+
+func (d *pbKeyData) Public() x509utils.PublicKey {
+	return d.pk.Public().(x509utils.PublicKey)
+}
+
+func (d *pbKeyData) Validate() error {
+	if v, ok := d.pk.(interface {
+		Validate() error
+	}); ok {
+		return v.Validate()
+	}
+	return nil
+}
+
+func (pb *PoolBuffer) addKeyUnlocked(fn string, pk x509utils.PrivateKey) error {
+	if pk != nil {
+		pd := &pbKeyData{
+			filename: fn,
+			pk:       pk,
+		}
+
+		if err := pd.Validate(); err != nil {
+			return err
+		}
+
+		// store
+		if pb.keys.keys == nil {
+			pb.keys.Reset()
+		}
+
+		pb.keys.keys.PushFront(pd)
+		pb.keys.count++
+	}
+	return nil
+}
