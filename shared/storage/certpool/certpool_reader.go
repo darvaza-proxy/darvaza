53e9893a38d8814e76335ab19525232b030f13c1
Alejandro Mery <amery@jpi.io>
2023-01-02 17:19:49 +0000

    shared/storage/certpool: Equal(), Minus(), Plus()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/storage/certpool/certpool_reader.go
+++ shared/storage/certpool/certpool_reader.go
@@ -64,3 +64,129 @@ func (s *CertPool) Get(_ context.Context, name string) (*x509.Certificate, error
 
 	return nil, fs.ErrNotExist
 }
+
+// Equal checks if another CertPooler is equal to this one
+func (s *CertPool) Equal(x x509utils.CertPooler) bool {
+	if x == nil {
+		// s != nil
+		return false
+	}
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if b, ok := x.(*CertPool); ok {
+		// same type
+		b.mu.RLock()
+		defer b.mu.RUnlock()
+
+		return equal(s, b)
+	}
+
+	// use StoreReader interface
+	hashed, count := len(s.hashed), 0
+	ctx := context.Background()
+
+	iter := func(cert *x509.Certificate) error {
+		// not larger
+		count++
+		if count > hashed {
+			return fs.ErrNotExist
+		}
+
+		// and all certs there exist here
+		h := HashCert(cert)
+		if _, found := s.hashed[h]; !found {
+			return fs.ErrNotExist
+		}
+
+		// continue
+		return nil
+	}
+
+	if err := x.ForEach(ctx, iter); err != nil {
+		// failed
+		return false
+	}
+	// same size
+	return count == hashed
+}
+
+func equal(a *CertPool, b *CertPool) bool {
+	// same size
+	if len(a.hashed) != len(b.hashed) {
+		return false
+	}
+	// and all hashes here exist there
+	for k := range a.hashed {
+		if _, ok := b.hashed[k]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Minus produces a new CertPool without any certificate on the given Pool
+func (s *CertPool) Minus(x x509utils.CertPooler) x509utils.CertPooler {
+	out := s.Clone().(*CertPool)
+
+	if x == nil {
+		// nothing to remove
+		return out
+	}
+
+	if b, ok := x.(*CertPool); ok {
+		// same type
+		b.mu.RLock()
+		defer b.mu.RUnlock()
+
+		return minus(out, b)
+	}
+
+	// use StoreReader interface
+	ctx := context.Background()
+	x.ForEach(ctx, func(cert *x509.Certificate) error {
+		out.deleteHash(HashCert(cert))
+		return nil
+	})
+
+	return out
+}
+
+func minus(out, b *CertPool) *CertPool {
+	for hash := range b.hashed {
+		_ = out.deleteHash(hash)
+	}
+	return out
+}
+
+// Plus produces a new CertPool with all certificate on the given Pool
+func (s *CertPool) Plus(x x509utils.CertPooler) x509utils.CertPooler {
+	out := s.Clone().(*CertPool)
+	if x != nil {
+		if b, ok := x.(*CertPool); ok {
+			// same type
+			b.mu.RLock()
+			defer b.mu.RUnlock()
+
+			return plus(out, b)
+		}
+
+		// use StoreReader interface
+		ctx := context.Background()
+		x.ForEach(ctx, func(cert *x509.Certificate) error {
+			out.addCertUnsafe(HashCert(cert), "", cert)
+			return nil
+		})
+	}
+
+	return out
+}
+
+func plus(out, b *CertPool) *CertPool {
+	for hash, cert := range b.hashed {
+		out.addCertUnsafe(hash, "", cert.cert)
+	}
+	return out
+}
