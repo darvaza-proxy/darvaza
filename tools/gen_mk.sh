f061d7133fc25faadb006a6d56068a2f52b9deea
Alejandro Mery <amery@jpi.io>
2023-01-13 19:34:25 +0000

    build-sys: use $(info ...) instead of echo for comments and simplify the generate call
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- tools/gen_mk.sh
+++ tools/gen_mk.sh
@@ -101,7 +101,7 @@ EOT
 		fi
 
 		cat <<EOT
-$cmd-$k:${depsx:+ $depsx}${deps:+ $(expand $cmd $deps)}
+$cmd-$k:${depsx:+ $depsx}${deps:+ $(expand $cmd $deps)} ; \$(info \$(M) $cmd: $k)
 $(echo "$callx" | sed -e "/^$/d;" -e "s|^|\t\$(Q) $cd|")
 
 EOT
