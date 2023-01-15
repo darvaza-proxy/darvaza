8d5fbedf4f44ee3790ecef90a5a335e6f33df599
Alejandro Mery <amery@jpi.io>
2023-01-15 05:35:11 +0000

    shared/net: export alias of net.IP for compatibility
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/net/std.go
+++ shared/net/std.go
@@ -7,6 +7,8 @@ import (
 type (
 	// Conn is an alias of the standard net.Conn type
 	Conn = net.Conn
+	// IP is an alias of the stadnard net.IP type
+	IP = net.IP
 	// Listener is an alias of the standard net.Listener type
 	Listener = net.Listener
 )
