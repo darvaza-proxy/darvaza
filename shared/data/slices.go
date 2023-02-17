c8df1e57a9d83b6fd93b6c926e4b73dec96faa42
Alejandro Mery <amery@jpi.io>
2023-02-17 15:48:53 +0000

    shared/data: changed SliceMinus[T]() arguments to start with a lowercase letter
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/data/slices.go
+++ shared/data/slices.go
@@ -3,21 +3,21 @@ package data
 
 // SliceMinus returns a new slice containing only the
 // elements of one slice not present on the second
-func SliceMinus[T comparable](A []T, B []T) []T {
-	return SliceMinusFn(A, B, func(a, b T) bool {
-		return a == b
+func SliceMinus[T comparable](a []T, b []T) []T {
+	return SliceMinusFn(a, b, func(va, vb T) bool {
+		return va == vb
 	})
 }
 
 // SliceMinusFn returns a new slice containing only elements
 // of slice A that aren't on slice B according to the callback
 // eq
-func SliceMinusFn[T any](A []T, B []T, eq func(T, T) bool) []T {
-	out := make([]T, 0, len(A))
+func SliceMinusFn[T any](a []T, b []T, eq func(T, T) bool) []T {
+	out := make([]T, 0, len(a))
 
 loop:
-	for _, v := range A {
-		for _, w := range B {
+	for _, v := range a {
+		for _, w := range b {
 			if eq(v, w) {
 				continue loop
 			}
