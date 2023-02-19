e2c8336bfeef2096fd1069a3817002e774490d46
Alejandro Mery <amery@jpi.io>
2023-02-19 03:28:45 +0000

    shared/data: add ListContains and ListContainsFn helpers
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/data/list.go
+++ shared/data/list.go
@@ -4,6 +4,27 @@ import (
 	"container/list"
 )
 
+// ListContains checks if a container/list contains an element
+func ListContains[T comparable](l *list.List, val T) bool {
+	return ListContainsFn(l, func(v T) bool {
+		return v == val
+	})
+}
+
+// ListContainsFn checks if a container/list contains an element
+// that satisfies a given function
+func ListContainsFn[T comparable](l *list.List, match func(T) bool) bool {
+	var found bool
+
+	if l != nil && match != nil {
+		ListForEach(l, func(v T) bool {
+			found = match(v)
+			return found
+		})
+	}
+	return found
+}
+
 // ListForEach calls a function for each value until told to stop
 func ListForEach[T any](l *list.List, fn func(v T) bool) {
 	if l == nil || fn == nil {
