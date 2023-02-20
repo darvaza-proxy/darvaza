6f8064b82cda2e2c44cf09914112f81f6be0ab0f
Alejandro Mery <amery@jpi.io>
2023-02-20 00:04:09 +0000

    shared/x509utils: add Hostname() and SantiseName() helper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/x509utils/names.go
+++ shared/x509utils/names.go
@@ -3,6 +3,9 @@ package x509utils
 import (
 	"crypto/x509"
 	"fmt"
+	"net"
+	"net/netip"
+	"net/url"
 	"strings"
 )
 
@@ -35,4 +38,39 @@ func Names(cert *x509.Certificate) ([]string, []string) {
 	}
 
 	return names, patterns
+}
+
+// Hostname returns a sanitised hostname for a parsed URL
+func Hostname(u *url.URL) (string, bool) {
+	return SanitiseName(u.Host)
+}
+
+// SanitiseName takes a Hostname and returns the name (or address)
+// we will use for matching certificates
+func SanitiseName(name string) (string, bool) {
+	if name != "" {
+		if host, _, err := net.SplitHostPort(name); err == nil {
+			if addr, err := netip.ParseAddr(host); err == nil {
+				// IP
+				addr.Unmap()
+				addr.WithZone("")
+				host = addr.String()
+			} else {
+				// Name
+				host = removeZone(host)
+			}
+			return host, len(host) > 2
+		}
+	}
+	return "", false
+}
+
+func removeZone(name string) string {
+	idx := strings.LastIndexFunc(name, func(r rune) bool {
+		return r == '%'
+	})
+	if idx < 0 {
+		return name
+	}
+	return name[:idx]
 }
