f3e39a52ee7d75b16f6fcc03879d86bd1b4c7b80
Alejandro Mery <amery@jpi.io>
2023-02-23 23:46:43 +0000

    shared/net: add AddrPort() helper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/net/addrs.go
+++ shared/net/addrs.go
@@ -196,3 +196,41 @@ func GetInterfacesNames(except ...string) ([]string, error) {
 	}
 	return out, nil
 }
+
+// revive:disable:cognitive-complexity
+
+// AddrPort attempts to extract a netip.AddrPort from an object
+func AddrPort(v any) (netip.AddrPort, bool) {
+	if p, ok := v.(interface {
+		AddrPort() netip.AddrPort
+	}); ok {
+		return p.AddrPort(), true
+	}
+
+	if p, ok := v.(interface {
+		Addr() net.Addr
+	}); ok {
+		return AddrPort(p.Addr())
+	}
+
+	if p, ok := v.(interface {
+		RemoteAddr() net.Addr
+	}); ok {
+		return AddrPort(p.RemoteAddr())
+	}
+
+	switch addr := v.(type) {
+	case *net.TCPAddr:
+		if ip, ok := netip.AddrFromSlice(addr.IP); ok {
+			return netip.AddrPortFrom(ip, uint16(addr.Port)), true
+		}
+	case *net.UDPAddr:
+		if ip, ok := netip.AddrFromSlice(addr.IP); ok {
+			return netip.AddrPortFrom(ip, uint16(addr.Port)), true
+		}
+	}
+
+	return netip.AddrPort{}, false
+}
+
+// revive:enable:cognitive-complexity
