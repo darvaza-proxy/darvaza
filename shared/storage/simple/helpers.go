ecc43794701ca360fe110a47fd47e6c01a186fe9
Alejandro Mery <amery@jpi.io>
2023-03-05 03:58:50 +0000

    shared/storage/simple: just a simple TLS store built from a PoolBuffer
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ shared/storage/simple/helpers.go
@@ -0,0 +1,53 @@
+package simple
+
+import (
+	"container/list"
+	"crypto/tls"
+
+	"github.com/darvaza-proxy/core"
+	"github.com/darvaza-proxy/darvaza/shared/x509utils"
+)
+
+// FindSupportedInMap attempts to find a matching supported tls.Certificate
+// on a MapList
+func FindSupportedInMap(chi *tls.ClientHelloInfo,
+	name string, m map[string]*list.List) *tls.Certificate {
+	//
+	var out *tls.Certificate
+
+	if name == "" {
+		// no sanitied name provided, produce one
+		s, ok := x509utils.SanitiseName(chi.ServerName)
+		if !ok {
+			return nil
+		}
+		name = s
+	}
+
+	core.MapListForEach(m, name, func(c *tls.Certificate) bool {
+		if err := chi.SupportsCertificate(c); err == nil {
+			out = c
+		}
+
+		// stop on the first supported match
+		return out != nil
+	})
+
+	return out
+}
+
+func removeKeyless(certs []*tls.Certificate) []*tls.Certificate {
+	var j int
+	for i, c := range certs {
+		if c.PrivateKey == nil {
+			continue
+		}
+
+		if i != j {
+			certs[j] = c
+		}
+		j++
+	}
+
+	return certs[:j]
+}
