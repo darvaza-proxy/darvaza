8145564e71d730df83dbc52fde8fed9d54e29277
Nagy Károly Gábriel <k@jpi.io>
2023-03-17 19:37:25 +0200

    shared/proxy: implement simple forwarder
    Signed-off-by: Nagy Károly Gábriel <k@jpi.io>

---
+++ shared/proxy/forwarder.go
@@ -0,0 +1,58 @@
+// Package proxy implements various proxy related utilities
+package proxy
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/netip"
+
+	"github.com/darvaza-proxy/core"
+)
+
+// Forward will take a context, a "downstream" net.Conn and a netip.Addr it will
+// create a new connection "upstream" and it will move bytes between the two.
+// Practically it will proxy between the two connections
+func Forward(ctx context.Context, conn net.Conn, addr netip.AddrPort) error {
+	defer conn.Close()
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+	}
+
+	if !addr.IsValid() {
+		return fmt.Errorf("invalid upstream address")
+	}
+
+	upstream, err := net.Dial("tcp", addr.String())
+	if err != nil {
+		// TODO: Need to retry?
+		return err
+	}
+	defer upstream.Close()
+
+	var wg core.WaitGroup
+
+	wg.Go(func() error {
+		// We just want to CloseWrite to signal that we finished writting
+		// but naked net.Conn does not have it so we transform to TCPConn
+		if cw, ok := conn.(interface{ CloseWrite() error }); ok {
+			defer cw.CloseWrite()
+		}
+		_, err = io.Copy(conn, upstream)
+		return err
+	})
+
+	if cwu, ok := upstream.(interface{ CloseWrite() error }); ok {
+		defer cwu.CloseWrite()
+	}
+	_, err = io.Copy(upstream, conn)
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+	return nil
+}
