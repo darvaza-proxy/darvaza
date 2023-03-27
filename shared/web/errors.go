7ac961142841235de54bd414c412c4b835387ed4
Alejandro Mery <amery@jpi.io>
2023-03-27 22:49:45 +0000

    shared/web: extend HTTPError implementing a primitive http.Handler
    and attempt to use middleware.ErrorHandler() when available
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/web/errors.go
+++ shared/web/errors.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 
 	"github.com/darvaza-proxy/core"
+	"github.com/darvaza-proxy/middleware"
 )
 
 var (
 	_ Error            = (*HTTPError)(nil)
 	_ core.Unwrappable = (*HTTPError)(nil)
+	_ http.Handler     = (*HTTPError)(nil)
 )
 
 // Error is an error that knows its HTTP Status Code
@@ -34,6 +36,37 @@ func (err *HTTPError) Status() int {
 		return http.StatusInternalServerError
 	default:
 		return err.Code
+	}
+}
+
+// ServeHTTP is a very primitive handler that will try to pass the error
+// to a [middleware.ErrorHandlerFunc] provided via the request's context.Context
+func (err *HTTPError) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if h, ok := middleware.ErrorHandler(req.Context()); ok {
+		// pass over to the error handler
+		h(rw, req, err)
+		return
+	}
+
+	code := err.Status()
+
+	switch {
+	case code == http.StatusOK:
+		rw.WriteHeader(http.StatusNoContent)
+	case code < http.StatusBadRequest:
+		rw.WriteHeader(code)
+	default:
+		hdr := rw.Header()
+		hdr["Content-Type"] = []string{"text/plain; charset=UTF-8"}
+
+		rw.WriteHeader(code)
+		fmt.Fprintln(rw, ErrorText(code))
+
+		if err.Err != nil {
+			if msg := err.Err.Error(); msg != "" {
+				fmt.Fprint(rw, "\n", msg)
+			}
+		}
 	}
 }
 
