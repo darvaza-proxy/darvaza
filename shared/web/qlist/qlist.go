67255c6a8b8010f2dbf6029146c559ce77be5ef4
Alejandro Mery <amery@jpi.io>
2023-03-28 23:17:58 +0000

    shared/web/qlist: set Epsilon to 0.001 as required by RFC9110
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/web/qlist/qlist.go
+++ shared/web/qlist/qlist.go
@@ -11,9 +11,9 @@ import (
 )
 
 const (
-	MinimumQuality = 0.   // MinimumQuality is 0
-	MaximumQuality = 1.   // MaximumQuality is 1
-	Epsilon        = 0.01 // Epsilon is the accuracy of the quality, 1/100
+	MinimumQuality = 0.    // MinimumQuality is 0
+	MaximumQuality = 1.    // MaximumQuality is 1
+	Epsilon        = 0.001 // Epsilon is the accuracy of the quality, 1/100
 )
 
 // QualityValue is a parsed item of a [QualityList]
