da7df06f15804e04d2f7ef24b1ab18df9719cb28
Alejandro Mery <amery@jpi.io>
2023-03-29 02:50:00 +0000

    shared/web/qlist: add support for attributes other than q
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/web/qlist/qlist.go
+++ shared/web/qlist/qlist.go
@@ -21,6 +21,7 @@ const (
 type QualityValue struct {
 	value   []string
 	quality float32
+	attrs   map[string]string
 }
 
 // IsZero tells if the QualityValue doesn't hold any information
@@ -45,11 +46,33 @@ func (q QualityValue) Quality() float32 {
 	return q.quality
 }
 
-func (q QualityValue) String() string {
-	if q.quality+Epsilon > MaximumQuality {
-		return q.Value()
+// Get retrieves the value of an attribute
+func (q QualityValue) Get(attr string) (string, bool) {
+	switch {
+	case attr == "q":
+		return fmt.Sprint(q.quality), true
+	case len(q.attrs) == 0:
+		return "", false
+	default:
+		v, ok := q.attrs[attr]
+		return v, ok
 	}
-	return fmt.Sprintf("%s;q=%v", q.Value(), q.quality)
+}
+
+func (q QualityValue) String() string {
+	var s []string
+
+	s = append(s, q.Value())
+
+	if MaximumQuality > q.quality+Epsilon {
+		s = append(s, fmt.Sprintf("q=%v", q.quality))
+	}
+
+	for k, v := range q.attrs {
+		s = append(s, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	return strings.Join(s, ";")
 }
 
 // QualityList is a list of [QualityValue]
@@ -115,7 +138,7 @@ func ParseQualityValue(s string) (QualityValue, error) {
 		// value
 		v := strings.ToLower(fields[0])
 		// attributes
-		q, ok := qualityAttribute(fields[1:])
+		q, m, ok := parseAttributes(fields[1:])
 
 		if len(v) == 0 || !ok {
 			err := fmt.Errorf("invalid argument: %q", s)
@@ -131,6 +154,7 @@ func ParseQualityValue(s string) (QualityValue, error) {
 		out = QualityValue{
 			value:   value,
 			quality: q,
+			attrs:   m,
 		}
 	}
 
@@ -156,24 +180,86 @@ func splitFields(s string) []string {
 	return fields
 }
 
-func qualityAttribute(attrs []string) (float32, bool) {
-	for _, s := range attrs {
-		if strings.HasPrefix(s, "q=") {
-			v := s[2:]
-			q, err := strconv.ParseFloat(v, 32)
-			switch {
-			case v == "":
-			case err != nil || math.IsNaN(q) || math.IsInf(q, 0):
-				return 0., false
-			case q < MinimumQuality+Epsilon:
-				return MinimumQuality, true
-			case q+Epsilon > MaximumQuality:
-				return MaximumQuality, true
-			default:
-				q = math.Round(q/Epsilon) * Epsilon
-				return float32(q), true
+func parseAttributes(attrs []string) (q float32, m map[string]string, ok bool) {
+	q = MaximumQuality
+	ok = true
+
+	for _, attr := range attrs {
+		if attr != "" {
+			q, m, ok = parseAttribute(attr, q, m)
+
+			if !ok {
+				break
 			}
 		}
 	}
-	return MaximumQuality, true
+
+	return q, m, ok
+}
+
+func parseAttribute(attr string,
+	prev float32, m map[string]string) (float32, map[string]string, bool) {
+	//
+	q, ok := prev, false
+	k, v := splitAttribute(attr)
+	switch k {
+	case "":
+	case "q":
+		if v != "" {
+			// parse quality
+			q, ok = parseQuality(v)
+		} else {
+			// skip empty quality
+			ok = true
+		}
+	default:
+		// regular
+		if m == nil {
+			m = make(map[string]string, 1)
+		}
+		ok = true
+		m[k] = v
+	}
+
+	return q, m, ok
+}
+
+func splitAttribute(s string) (key, value string) {
+	// find delimiter
+	i := strings.IndexFunc(s, func(r rune) bool {
+		return r == '='
+	})
+
+	// split
+	switch {
+	case i < 0:
+		key = s
+	default:
+		key, value = s[:i], s[i+1:]
+	}
+
+	// sanitise
+	key = strings.ToLower(strings.TrimSpace(key))
+	value = strings.ToLower(strings.TrimSpace(value))
+	return key, value
+}
+
+func parseQuality(v string) (float32, bool) {
+	ok := true
+	q, err := strconv.ParseFloat(v, 32)
+	switch {
+	case err != nil || math.IsNaN(q) || math.IsInf(q, 0):
+		ok = false
+	case q < MinimumQuality+Epsilon:
+		q = MinimumQuality
+	case q+Epsilon > MaximumQuality:
+		q = MaximumQuality
+	default:
+		q = math.Round(q/Epsilon) * Epsilon
+	}
+
+	if !ok {
+		return 0, false
+	}
+	return float32(q), true
 }
