dd3a4cb224a507790e8ee342a35342e85df43dda
Alejandro Mery <amery@jpi.io>
2023-03-31 13:29:14 +0000

    shared/web: WithErrorHandler/ErrorHandler based on core.NewContextKey
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ shared/web/error_handler.go
@@ -0,0 +1,26 @@
+package web
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/darvaza-proxy/core"
+)
+
+// ErrorHandlerFunc is the signature of a function used as ErrorHandler
+type ErrorHandlerFunc func(http.ResponseWriter, *http.Request, error)
+
+// WithErrorHandler attaches an ErrorHandler function to a context
+// for later retrieval
+func WithErrorHandler(ctx context.Context, h ErrorHandlerFunc) context.Context {
+	return errCtxKey.WithValue(ctx, h)
+}
+
+// ErrorHandler attempts to pull an ErrorHandler from the context.Context
+func ErrorHandler(ctx context.Context) (ErrorHandlerFunc, bool) {
+	return errCtxKey.Get(ctx)
+}
+
+var (
+	errCtxKey = core.NewContextKey[ErrorHandlerFunc]("ErrorHandler")
+)
