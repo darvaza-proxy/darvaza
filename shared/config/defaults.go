eee04a2fcd4a42ad1ab6461dc810bce1c9cd5d6e
Alejandro Mery <amery@jpi.io>
2023-06-12 12:34:12 +0100

    shared/config: add CanUpdate() wrapper
    CanUpdate tells when the given value is the zero value of that type
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/config/defaults.go
+++ shared/config/defaults.go
@@ -7,3 +7,8 @@ import "github.com/amery/defaults"
 func SetDefaults(v any) error {
 	return defaults.Set(v)
 }
+
+// CanUpdate returns true when the given value is an initial value of its type
+func CanUpdate(v any) bool {
+	return defaults.CanUpdate(v)
+}
