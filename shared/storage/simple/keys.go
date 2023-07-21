7ac2693f735a60a76cf0397d250dc5327f2b1589
Alejandro Mery <amery@jpi.io>
2023-07-21 21:08:07 +0000

    shared/storage/simple: add HasKey()/HasPublicKey()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/storage/simple/keys.go
+++ shared/storage/simple/keys.go
@@ -1,6 +1,8 @@
 package simple
 
 import (
+	"crypto"
+
 	"darvaza.org/core"
 	"darvaza.org/darvaza/shared/x509utils"
 )
@@ -30,6 +32,40 @@ func (s *Store) AddKey(pk x509utils.PrivateKey) error {
 	}
 
 	return nil
+}
+
+// HasKey checks if the store contains the specified PrivateKey.
+func (s *Store) HasKey(pk crypto.PrivateKey) bool {
+	if pk != nil {
+		s.lockInit()
+		defer s.mu.Unlock()
+
+		for _, pk1 := range s.keys {
+			if pk1.Equal(pk) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// HasPublicKey checks if the store contains a PrivateKey matching
+// the given PublicKey.
+func (s *Store) HasPublicKey(pub crypto.PublicKey) bool {
+	if pub != nil {
+		s.lockInit()
+		defer s.mu.Unlock()
+
+		for _, pk1 := range s.keys {
+			pub1, ok := pk1.Public().(x509utils.PublicKey)
+			if ok && pub1.Equal(pub) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 // Keys returns a copy of the slice containing the stored private keys.
