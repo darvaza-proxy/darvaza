746a64f83438d43e8a70ab6fa2fe4aad130d4b9d
Alejandro Mery <amery@jpi.io>
2023-09-20 16:13:46 +0000

    agent/httpserver: add dummy QuicHeadersMiddleware for go 1.19
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- agent/httpserver/http3_old.go
+++ agent/httpserver/http3_old.go
@@ -2,7 +2,14 @@
 
 package httpserver
 
+import "net/http"
+
 // TODO: add mechanism to disable HTTP/3 support instead
 var (
 	_ int = "unfortunately your version of Go can't be supported"
 )
+
+// QuicHeaderMiddleware does nothing when building with go 1.19 or older
+func (*Server) QuicHeadersMiddleware(next http.Handler) http.Handler {
+	return next
+}
