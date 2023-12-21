71bb0c1e2067c634aff66c2db2e79141563f5f1c
Alejandro Mery <amery@jpi.io>
2023-12-21 20:03:48 +0000

    shared/config: fix SetDefaults() to call `SetDefaults() error` directly when available
    defaults.Set(v) won't use that methods on the entry call
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shared/config/defaults.go
+++ shared/config/defaults.go
@@ -5,6 +5,12 @@ import "github.com/amery/defaults"
 // SetDefaults applies `defaults` structtags and SetDefaults()
 // recursively
 func SetDefaults(v any) error {
+	if t, ok := v.(defaults.SetterWithError); ok {
+		// defaults.Set will omit this setter on
+		// the entry function to prevent loops.
+		return t.SetDefaults()
+	}
+
 	return defaults.Set(v)
 }
 
